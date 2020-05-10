3a78f8c86cdd4e6bd50e508a1c23d968e57cb579
Dicky Alamsyah <dickyalamsyah0901@gmail.com>
2020-05-10 18:41:03 +0700

    init project

---
+++ app/src/main/java/com/example/dicky/project/Mainactivity.kt
@@ -0,0 +1,12 @@
+package com.example.dicky.project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Mainactivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainactivity)
+    }
+}
